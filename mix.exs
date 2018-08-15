e11ccefe4745c2519e27ac0e1efbbdea2d511e15
Ben Le Cam <ben@bleachr.co>
2018-08-15 10:58:04 -0500

    Fixes warning issues for mix file for function call parentheses

--- mix.exs
+++ mix.exs
@@ -7,9 +7,9 @@ defmodule NaturalSort.Mixfile do
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps,
-     description: description,
-     package: package
+     deps: deps(),
+     description: description(),
+     package: package()
    ]
   end
 
